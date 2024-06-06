7a59cd0a929e01fa7cab9abc410191e2b4cd09cc
GAlibrz <gitalibagherzadeh@gmail.com>
2024-06-06 12:31:06 +0200

    Project pan updated, data report added, data pipeline completed

--- project/csv2db.py
+++ project/csv2db.py
@@ -1,45 +1,205 @@
 import os
 import sqlite3
 import pandas as pd
-
+import gc
 from kaggle.api.kaggle_api_extended import KaggleApi
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import grangercausalitytests
+
+stock_start_date = '1998-01-01'
+stock_end_date = '2020-07-01'
+stock_columns_to_keep = ['Date', 'price']
+tempreture_columns_to_keep = ['Area', 'Months','Months Code' , 'Year', 'Value']
+
+def stock_df_date_handler(df, date1, date2):
+
+    df = df[df['Date'] > date1]
+    df = df[df['Date'] < date2]
+    return df
+
+def stock_df_make_montly(df):
+
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['index_Date'] = df['Date']
+    df.set_index('index_Date', inplace=True)
+
+    monthly_df = df.resample('M').agg({
+    'Date' : 'last',
+    'Open': 'first',
+    'High': 'max',
+    'Low': 'min',
+    'Close': 'last',
+    'Volume': 'sum'
+        })
+
+    monthly_df['Date'] = monthly_df['Date'].dt.strftime('%Y-%m')
+    return monthly_df
+
+def stock_df_column_handler(df,columns):
+
+    df['price'] = (df['Open'] + df['Close']) / 2
+    df = df.filter(items = columns)
+    return df
+
 
 
 kaggleApi = KaggleApi()
 kaggleApi.authenticate()
-datasetUrl = 'aayushmishra1512/faang-complete-stock-data'
+marketDatasetUrl = 'aayushmishra1512/faang-complete-stock-data'
+tempretureDatasetUrl = 'sevgisarac/temperature-change'
 
 currentDirectory = os.path.dirname(__file__)
 parentDirectory = os.path.dirname(currentDirectory)
 datasetDirectory = os.path.join(parentDirectory, 'data')
-kaggleApi.dataset_download_files(datasetUrl, path = datasetDirectory, unzip = True)
+kaggleApi.dataset_download_files(marketDatasetUrl, path = datasetDirectory, unzip = True)
+kaggleApi.dataset_download_files(tempretureDatasetUrl, path = datasetDirectory, unzip = True)
+
 
 Amazon_csv_path = os.path.join(datasetDirectory, 'Amazon.csv')
 Apple_csv_path = os.path.join(datasetDirectory, 'Apple.csv')
-Facebook_csv_path = os.path.join(datasetDirectory, 'Facebook.csv')
-Google_csv_path = os.path.join(datasetDirectory, 'Google.csv')
-Netflix_csv_path = os.path.join(datasetDirectory, 'Netflix.csv')
+tempreture_csv_path = os.path.join(datasetDirectory, 'FAOSTAT_data_1-10-2022.csv')
 
 
 try: 
+    
     Amazon_df = pd.read_csv(Amazon_csv_path)
+    Amazon_df = stock_df_make_montly(Amazon_df)
+    Amazon_df = stock_df_date_handler(Amazon_df, stock_start_date, stock_end_date)
+    Amazon_df = stock_df_column_handler(Amazon_df, stock_columns_to_keep)
+
     Apple_df = pd.read_csv(Apple_csv_path)
-    Facebook_df = pd.read_csv(Facebook_csv_path)
-    Google_df = pd.read_csv(Google_csv_path)
-    Netflix_df = pd.read_csv(Netflix_csv_path)
+    Apple_df = stock_df_make_montly(Apple_df)
+    Apple_df = stock_df_date_handler(Apple_df, stock_start_date, stock_end_date)
+    Apple_df = stock_df_column_handler(Apple_df, stock_columns_to_keep)
 
-    os.makedirs(os.path.dirname("../data/stocks.db"), exist_ok=True)
-    connection = sqlite3.connect("../data/stocks.db")
+    final_stock_df = Amazon_df[['Date', 'price']]
+    final_stock_df = final_stock_df.rename(columns = {'price' : 'Amazon_price'})
+    final_stock_df['Apple_price'] = Apple_df['price']
+    final_stock_df = final_stock_df.reset_index(drop = True)
+    #final_stock_df.to_csv('stocks.csv', index=False)
 
-    Amazon_df.to_sql("Amazon", connection, if_exists="replace", index=False)
-    Apple_df.to_sql("Apple", connection, if_exists="replace", index=False)
-    Facebook_df.to_sql("Facebook", connection, if_exists="replace", index=False)
-    Google_df.to_sql("Google", connection, if_exists="replace", index=False)
-    Netflix_df.to_sql("Netflix", connection, if_exists="replace", index=False)
+    tempreture_df = pd.read_csv(tempreture_csv_path)
+    tempreture_df = tempreture_df.filter(items = tempreture_columns_to_keep)
+    #print(tempreture_df.dtypes)
+    #print('countries before', len(tempreture_df['Area'].unique()))
+    #print(len(tempreture_df))
+    tempreture_df = tempreture_df[tempreture_df['Months Code'] < 7013]
+    #print('here')
+    #print(len(tempreture_df))
+
+    #print('countries After', len(tempreture_df['Area'].unique()))
+
+    '''
+        *****************************************************
+    '''
+
+    month_map = {
+        'January': '01', 'February': '02', 'March': '03', 'April': '04',
+        'May': '05', 'June': '06', 'July': '07', 'August': '08',
+        'September': '09', 'October': '10', 'November': '11', 'December': '12'
+    }
+
+    # Apply month mapping
+    tempreture_df['Month'] = tempreture_df['Months'].map(month_map)
+    #print('*****************************************')
+    #print(tempreture_df)
+    #print('*****************************************')
 
 
+    # Combine 'Year' and 'Month' to create a date column
+    tempreture_df['Date'] = pd.to_datetime(tempreture_df['Year'].astype(str) + '-' + tempreture_df['Month'])
+
+    # Filter temperature data to keep relevant columns
+
+    # Aggregate the temperature data by date and area
+    tempreture_df = tempreture_df.groupby(['Date', 'Area']).agg({'Value': 'mean'}).reset_index()
+    #tempreture_df = tempreture_df.reset_index()
+    tempreture_df['Date'] = pd.to_datetime(tempreture_df['Date']).dt.strftime('%Y-%m')
+
+    print(final_stock_df.head())
+    print(tempreture_df.head())
+
+
+    '''
+        *****************************************************
+    '''
+
+    merged_df = pd.merge(final_stock_df, tempreture_df, on='Date', how='inner')
+
+    # Handle missing values
+    merged_df.dropna(inplace=True)
+
+    print(merged_df.head())
+
+    '''************************************************************'''
+
+    # Pivot the data frame to have countries as columns and dates as rows
+    pivot_df = tempreture_df.pivot(index='Date', columns='Area', values='Value')
+
+    # Compute the correlation matrix
+    countries_temp_correlation_matrix = pivot_df.corr()
+
+     #Flatten the correlation matrix to a 1D array
+    countries_temp_correlation_values = countries_temp_correlation_matrix.values.flatten()
+
+    # Remove self-correlation values (1s on the diagonal)
+    countries_temp_correlation_values = countries_temp_correlation_values[countries_temp_correlation_values != 1]
+
+    # Summary statistics
+    #summary_stats = pd.Series(countries_temp_correlation_values).describe()
+    #summary_stats
+
+    # Plotting the histogram
+    plt.figure(figsize=(10, 6))
+    plt.hist(countries_temp_correlation_values, bins=50, color='blue', alpha=0.7)
+    plt.title('Histogram of Correlation Coefficients')
+    plt.xlabel('Correlation Coefficient')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    #plt.show()
+
+
+
+    '''***************************************'''
+
+    tmp_stock_correlation_matrix = merged_df[['Amazon_price', 'Apple_price', 'Value']].corr()
+    print(tmp_stock_correlation_matrix)
+
+    # Plotting the correlation
+    plt.figure(figsize=(8, 6))
+    plt.matshow(tmp_stock_correlation_matrix, fignum=1)
+    plt.colorbar()
+    plt.xticks(range(len(tmp_stock_correlation_matrix.columns)), tmp_stock_correlation_matrix.columns, rotation=45)
+    plt.yticks(range(len(tmp_stock_correlation_matrix.columns)), tmp_stock_correlation_matrix.columns)
+    #plt.show()
+
+    '''*************************************************'''
+
+    data_for_granger = merged_df[['Amazon_price', 'Apple_price', 'Value']]
+
+    # Perform Granger causality test for Amazon price and temperature
+    max_lag = 12
+    granger_test_result = grangercausalitytests(data_for_granger[['Amazon_price', 'Value']], max_lag, verbose=False)
+
+
+# Display the results for each lag
+    for lag, results in granger_test_result.items():
+        print(f"Lag {lag}:")
+        for test, result in results[0].items():
+            
+            print(f"  {test} - F-statistic: {result[0]:.4f}, p-value: {result[1]:.4f}")
+    '''*************************************************************'''
+
+    os.makedirs(os.path.dirname("../data/stocks_temp.db"), exist_ok = True)
+    connection = sqlite3.connect("../data/stocks_temp.db")
+    #final_stock_df.to_sql('stocks', connection, if_exists = "replace", index = False)
+    #tempreture_df.to_sql('tempreture', connection, if_exists = "replace", index = False)
+
+    
     connection.close()
     print("Done.")
 
-except:
-    print("OOps")
+except Exception as e:
+
+    print(f"Error: {e}")
+
--- project/project-plan.md
+++ project/project-plan.md
@@ -1,22 +1,29 @@
 # Project Plan
 
 ## Title
-FAANG Stock Analysis Project
+Apple and Amazon Stock Performance Effects on Climate Change Project
 
-## Main Question
-Does analyzing historical stock data of FAANG companies provide insights into their financial performance and market trends?
+## Main Questions
+1- Is there a correlation between stock market performance and temperature changes?
+2- Can stock market prices predict future temperature changes?
 
 ## Description
-This project aims to analyze historical stock data of Facebook, Amazon, Apple, Netflix, and Google (FAANG) to gain insights into their financial performance and market trends. By examining factors such as opening and closing prices, trading volumes, and other relevant metrics, the project seeks to understand patterns, correlations, and potential predictors of stock price movements.
+This project aims to investigate the potential relationship between the stock market performance of huge tech companies like Amazon and Apple and temperature changes across various countries, exploring if stock prices can predict future temperature changes. The idea is based on the notion that generating more revenue might increase environmental pollution, thus resulting in higher temperatures on Earth.
 
 
-### Datasource: FAANG Complete DataSet
+### Datasource: FAANG DataSet
 * Metadata URL: https://www.kaggle.com/datasets/aayushmishra1512/faang-complete-stock-data
 * Data URL: https://www.kaggle.com/datasets/aayushmishra1512/faang-complete-stock-data
 * Data Type: CSV
+* License: Public Available 
 Short description: This CSV file contains historical stock data of Facebook, Amazon, Apple, Netflix and Google including opening price, closing price and trading volume.
 
-
+### Datasource: FAANG DataSet
+* Metadata URL: https://www.kaggle.com/datasets/sevgisarac/temperature-change
+* Data URL: https://www.kaggle.com/datasets/sevgisarac/temperature-change
+* Data Type: CSV
+* Licencse: CC0 - Public 
+Short description: The FAOSTAT Temperature Change domain disseminates statistics of mean surface temperature change by country, with annual updates. The current dissemination covers the period 1961–2020. 
 
 
 ## Work Packages
@@ -25,7 +32,7 @@ Short description: This CSV file contains historical stock data of Facebook, Ama
 1. Data Collection and Preprocessing [Issue #1 ][i1]
 2. Exploratory Data Analysis [Issue #2 ][i2]
 3. Statistical Analysis and Correlation Study [Issue #3 ][i3]
-4. Predictive Modeling [Issue #4 ][i4]
+4. Statistic Modeling [Issue #4 ][i4]
 5. Report and Visualization [Issue #5 ][i5]
 
 [i1]: https://github.com/GAlibrz/MADE-Project/issues/1
